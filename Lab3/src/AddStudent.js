c41504a243c1373a735010b810e0b83537c5a73d
Majmos <mimsowy@gmail.com>
2022-04-23 14:31:57 +0200

    minor changes

--- Lab3/src/AddStudent.js
+++ Lab3/src/AddStudent.js
@@ -1,12 +1,24 @@
 function AddStudent() {
 
     return (
-        <div>
-            <form>
-                <div><label>Nazwa:</label><input type={"text"}></input></div>
-                <div><label>e-mail:</label><input type={"email"}></input></div>
-            </form>
-        </div>
+        <form>
+            <div className="formBox">
+                <label className="formLabel">Nowe ogłoszenie</label>
+                <label>
+                    Nazwa:
+                    <input placeholder="Nazwa" type={"text"}/>
+                </label>
+                <input placeholder="E-mail" type={"email"}/>
+                <textarea placeholder="Opis"/>
+                <div>
+                    <input placeholder="Tag" type={"text"}/><button className="formButton">Dodaj</button>
+                </div>
+                <div>
+                    <input placeholder="Przedmiot" type={"text"}/><button className="formButton">Dodaj</button>
+                </div>
+            </div>
+            <input className="submitButton" value={"Dodaj ogłoszenie"} type={"submit"}/>
+        </form>
     );
 }
 
