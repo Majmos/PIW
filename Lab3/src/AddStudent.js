6ff6087bddc2f604382caecaa9c75817e3d3f1df
Majmos <mimsowy@gmail.com>
2022-04-23 23:47:44 +0200

    style improvements

--- Lab3/src/AddStudent.js
+++ Lab3/src/AddStudent.js
@@ -2,21 +2,29 @@ function AddStudent() {
 
     return (
         <div className="formBox">
-            <h1 className="formLabel">Nowe ogłoszenie</h1>
+            <h1>Nowe ogłoszenie</h1>
             <form>
-                <div>  
-                    <label>
-                        Nazwa:
-                        <input placeholder="Nazwa" type={"text"}/>
-                    </label>
+                <div className="formElement">  
+                    <label>Nazwa:</label>
+                    <input placeholder="Nazwa" type={"text"}/>
                 </div>
-                <div><input placeholder="E-mail" type={"email"}/></div>
-                <div><textarea placeholder="Opis"/></div>
-                <div>
-                    <input placeholder="Tag" type={"text"}/><button className="formButton">Dodaj</button>
+                <div className="formElement">
+                    <label>E-mail:</label>
+                    <input placeholder="E-mail" type={"email"}/></div>
+                <div className="formElement">
+                    <label>Opis:</label>
+                    <textarea placeholder="Opis"/></div>
+                <div className="formElement">
+                    <label>Tag:</label>
+                    <div className="inputBox">
+                        <input placeholder="Tag" type={"text"}/><button className="formButton">Dodaj</button>
+                    </div>
                 </div>
-                <div>
-                    <input placeholder="Przedmiot" type={"text"}/><button className="formButton">Dodaj</button>
+                <div className="formElement">
+                    <label>Przedmiot:</label>
+                    <div className="inputBox">
+                        <input placeholder="Przedmiot" type={"text"}/><button className="formButton">Dodaj</button>
+                    </div>
                 </div>
                 <input className="submitButton" value={"Dodaj ogłoszenie"} type={"submit"}/>
             </form>
