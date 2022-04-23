a6ac1a56440044b795c50618975a543c6aaf40d3
Majmos <mimsowy@gmail.com>
2022-04-23 14:41:38 +0200

    work in progress

--- Lab3/src/AddStudent.js
+++ Lab3/src/AddStudent.js
@@ -1,24 +1,26 @@
 function AddStudent() {
 
     return (
-        <form>
-            <div className="formBox">
-                <label className="formLabel">Nowe ogłoszenie</label>
-                <label>
-                    Nazwa:
-                    <input placeholder="Nazwa" type={"text"}/>
-                </label>
-                <input placeholder="E-mail" type={"email"}/>
-                <textarea placeholder="Opis"/>
+        <div className="formBox">
+            <h1 className="formLabel">Nowe ogłoszenie</h1>
+            <form>
+                <div>  
+                    <label>
+                        Nazwa:
+                        <input placeholder="Nazwa" type={"text"}/>
+                    </label>
+                </div>
+                <div><input placeholder="E-mail" type={"email"}/></div>
+                <div><textarea placeholder="Opis"/></div>
                 <div>
                     <input placeholder="Tag" type={"text"}/><button className="formButton">Dodaj</button>
                 </div>
                 <div>
                     <input placeholder="Przedmiot" type={"text"}/><button className="formButton">Dodaj</button>
                 </div>
-            </div>
-            <input className="submitButton" value={"Dodaj ogłoszenie"} type={"submit"}/>
-        </form>
+                <input className="submitButton" value={"Dodaj ogłoszenie"} type={"submit"}/>
+            </form>
+        </div>
     );
 }
 
