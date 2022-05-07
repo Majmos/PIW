0e13320274350808b5b39cafb9d2730a5d9d9138
Majmos <mimsowy@gmail.com>
2022-05-08 01:33:29 +0200

    Student profiles made prettier

--- Lab3/src/components/Students/StudentProfile.js
+++ Lab3/src/components/Students/StudentProfile.js
@@ -1,4 +1,5 @@
 import { useParams, Link } from "react-router-dom";
+import Student from "./Student";
 
 const StudentProfile = ({ getStudent }) => {
 
@@ -7,31 +8,11 @@ const StudentProfile = ({ getStudent }) => {
   const student = getStudent(studentid);
 
   return (
-    <div>
-      <div>
-        <img src={student.imgUrl} alt="Zdjęcie profilowe" />
+    <div className="student-profile">
+      <div className="student">
+        <Student student={student} />
+        <Link to="/sendMessage"><button className="submitButton">Wyślij wiadomość</button></Link>
       </div>
-      <div>
-        <h2>Nazwa:</h2>
-        {student.name}
-      </div>
-      <div>
-        <h2>Opis:</h2>
-        {student.description}
-      </div>
-      <div>
-        <h2>Tags:</h2>
-        <ul className="tags">
-          {student.tags.map((tag, i) => <li key={i} className="tag">{tag}</li>)}
-        </ul>
-      </div>
-      <div>
-        <h2>Subjects:</h2>
-        <ul className="subjects">
-          {student.subjects.map((subject, i) => <li key={i} className="subject">{subject}</li>)}
-        </ul>
-      </div>
-      <Link to="/sendMessage"><button className="submitButton">Wyślij wiadomość</button></Link>
     </div>
   );
 };
